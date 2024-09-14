e0276d7f6eb10861a76ac4bee014bf215a454649
Theomund <34360334+Theomund@users.noreply.github.com>
2024-09-14 17:11:55 +0000

    feat: add ping command to bot

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,36 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected the token in an environment variable");
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
 }
